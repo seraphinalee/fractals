cc2cd40eceb5656fe123a0c761f8630b1d87e9ae
Dylan King <kingda16@wfu.edu>
2017-06-26 10:45:52 -0400

    Started plotting

--- gasketgraph.m
+++ gasketgraph.m
@@ -1,6 +1,9 @@
-function [ output_args ] = Untitled9( input_args )
+function [ output ] = gasketgraph( gasketdata )
 %UNTITLED9 Summary of this function goes here
 %   Detailed explanation goes here
+f0 = @(x) 1/2(x-[0.5,tan(60)*0.5]) + [0.5,tan(60)*0.5];
+f1 = @(x) 1/2(x-[1,0]) + [1,0];
+f2 = @(x) 1/2(x-[0,1]) + [0,1];
 
 
 end
