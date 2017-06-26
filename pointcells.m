54a2103e7397868c88f8dbfa20c5c797da576b8e
Dylan King <kingda16@wfu.edu>
2017-06-26 14:45:50 -0400

    point to cell done

--- pointcells.m
+++ pointcells.m
@@ -1,7 +1,7 @@
-function [ output_args ] = Untitled2( input_args )
+function [ cell1 cell2 ] = pointcells( address )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-
-
+cell1 = [address(1); address(3:end)];
+cell2 = [address(2); address(3:end)];
 end
 
