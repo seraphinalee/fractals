fd6919ad5567ca955608ae52851553796b7c4e02
Dylan King <kingda16@wfu.edu>
2017-06-26 15:08:23 -0400

    Measure working

--- edgeresistance.m
+++ edgeresistance.m
@@ -1,4 +1,4 @@
-function res = edgeresistance( x, y, ra, rb )
+function res = edgeresistance( x, y, r0, r1 )
 %x and y are points
 %ra is the resistance on the outer cells, rb is on inner cells
 
@@ -9,9 +9,9 @@ intersection = intersect(cells',cells2','rows')';
 res = 1;
 for i = 1:(length(intersection)-1)/2
     if intersection(2*i) == intersection(2*i+1)
-        res = res*ra;
+        res = res*r0;
     else
-        res = res*rb;
+        res = res*r1;
     end
 end
 
--- measure.m
+++ measure.m
@@ -1,13 +1,13 @@
 %takes in a cell of form [a_1, a_2, ...] where the cell = ...F_{a_2}F_{a_1}(SG)
 %mua is measure on the outer cells, mub is measure on the inner cells
 %3mua+6mub = 1
-function output = measure(cell, mua)
-mub = (1-3*mua)/6;
+function output = measure(cell, mu0)
+mu1 = (1-3*mu0)/6;
 output = 1;
 for i = 1:length(cell)/2
     if cell(2*i-1) == cell(2*i)
-        output = output*mua;
+        output = output*mu0;
     else
-        output = output*mub;
+        output = output*mu1;
     end
 end
