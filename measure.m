05ca608341204a2a8572d7826cf4b153243dfd5a
Dylan King <kingda16@wfu.edu>
2017-06-25 22:56:17 -0400

    Fixed naming in measure.m

--- measure.m
+++ measure.m
@@ -1,13 +1,13 @@
 %takes in a cell of form [a_1, a_2, ...] where the cell = ...F_{a_2}F_{a_1}(SG)
 %mua is measure on the outer cells, mub is measure on the inner cells
 %3mua+6mub = 1
-function measure = measure(cell, mua)
+function output = measure(cell, mua)
 mub = (1-3*mua)/6;
-measure = 1;
-for i = 1:len(cell)/2
-    if cell(2i-1) == cell(2i)
-        measure = measure*mua;
+output = 1;
+for i = 1:length(cell)/2
+    if cell(2*i-1) == cell(2*i)
+        output = output*mua;
     else
-        measure = measure*mub;
+        output = output*mub;
     end
 end
