0a6973a08a7e90979230a5a236f04474f42c43a7
Dylan King <kingda16@wfu.edu>
2017-06-26 15:01:25 -0400

    Working resistance

--- edgeresistance.m
+++ edgeresistance.m
@@ -2,35 +2,13 @@ function res = edgeresistance( x, y, ra, rb )
 %x and y are points
 %ra is the resistance on the outer cells, rb is on inner cells
 
-neighbors = pointneighbors(x);
-neighbors2 = pointneighbors(y);
-is_neighbor = false;
-
-for i=1:len(neighbors)
-    if all(neighbors(i)==y)
-        is_neighbor = true;
-    end
-end
-
-if not(is_neighbor)
-    res = 0;
-    return
-end
-z = [];
-for i=1:len(neighbors)
-    for j=1:len(neighbors2)
-        if all(neighbors(i) == neighbors2(j))
-            z = neighbors(i);
-        end
-    end
-end
-
-%get cell that has x, y, z
-%and then do below calculations on that cell
+cells = pointneighbors(x);
+cells2 = pointneighbors(y);
+intersection = intersect(cells',cells2','rows')';
 
 res = 1;
-for i = 1:(len(x)-1)/2
-    if x(2i) == x(2i+1)
+for i = 1:(length(intersection)-1)/2
+    if intersection(2*i) == intersection(2*i+1)
         res = res*ra;
     else
         res = res*rb;
--- gasketcore.m
+++ gasketcore.m
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 m=6
-=======
-m=9
->>>>>>> 99a59294f97040f2b11a7a2ae39b355d0b6e7845
 gasket_points = [[0;0] [0;1] [1;1] [1;2] [2;2] [2;0]]
 for i = 1:m-1
     gasket_points = repelem(gasket_points,1,3);
