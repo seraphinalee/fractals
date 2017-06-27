0efcd6b5c927d4ce42a264a491b18752263f9cf5
Dylan King <kingda16@wfu.edu>
2017-06-26 23:06:11 -0400

    A scanning system, and comments

--- edgeresistance.m
+++ edgeresistance.m
@@ -1,11 +1,14 @@
 function res = edgeresistance( x, y, r0, r1 )
 %x and y are points
-%ra is the resistance on the outer cells, rb is on inner cells
+%r0 is the resistance on the outer cells, r1 is on inner cells
 
+%find the two neighboring cells for each neighbor, and the intersection
 cells = pointcells(x);
 cells2 = pointcells(y);
 intersection = intersect(cells',cells2','rows')';
 
+%iterate through by pairs of addresses (first is q)
+%same maps gives r0, diff gives r1
 res = 1;
 for i = 1:(length(intersection)-1)/2
     if intersection(2*i) == intersection(2*i+1)
--- gasketgraph.m
+++ gasketgraph.m
@@ -1,13 +1,19 @@
 function [X Y Z] = gasketgraph( gasketdata )
-%UNTITLED9 Summary of this function goes here
-%   Detailed explanation goes here
+%takes a row vector of column vectors
+%each column vector is a standard address with a z-value appended on the
+%end
+
+%declaring empty output
 X = zeros(length(gasketdata),1);
 Y = zeros(length(gasketdata),1);
 Z = zeros(length(gasketdata),1);
+%defining IFS
 f0 = @(x) 1/2*(x-[0.5,tan(60)*0.5]) + [0.5,tan(60)*0.5];
 f1 = @(x) 1/2*(x-[1,0]) + [1,0];
 f2 = @(x) 1/2*(x-[0,0]) ;
+%for each point...
 for i = 1:length(gasketdata)
+    %find the proper qi...
     if gasketdata(1,i) == 0
         cartesian = [0.5,tan(60)*0.5];
     end
@@ -17,6 +23,7 @@ for i = 1:length(gasketdata)
     if gasketdata(1,i) == 2
         cartesian = [0,0];
     end
+    %...and iterate the funciton maps to get the actual point
     for j = 2:length(gasketdata(:,i))-1
        if gasketdata(j,i) == 0
            cartesian = f0(cartesian);
@@ -28,6 +35,7 @@ for i = 1:length(gasketdata)
            cartesian = f2(cartesian);
        end
     end
+    %insert this x,y,z triple into the cartesian vectors for output
     X(i,1) = cartesian(1);
     Y(i,1) = cartesian(2);
     Z(i,1) = gasketdata(end,i);
