905c693e283d1b6334416568192a020ea6686362
Dylan King <kingda16@charybdis2.math.cornell.edu>
2017-07-06 17:34:25 -0400

    before overnight run

--- unstable/interval.m
+++ unstable/interval.m
@@ -1,9 +1,9 @@
 
-m=5; %level
+m=2; %level
 
 
-p=0.5; %measure parameter
-q=0.5; %resistance parameter
+p=0.9; %measure parameter
+q=0.1; %resistance parameter
 
 measure = [p/2 (1-p)/2 (1-p)/2 p/2]; %base measure split
 resistance = [q/2 (1-q)/2 (1-q)/2 q/2]; %base resistance split
@@ -16,7 +16,7 @@ for i = 2:m
     resistance = repelem(resistance,4);
     resistance = resistance.*repmat([q/2 (1-q)/2 (1-q)/2 q/2],1,length(resistance)/4);
 end
-
+laplacian = zeros(4^m-1);
 for i = 1:4^m-1
     avgmeasure = (measure(i)+measure(i+1))/2;
     laplacian(i,i) = (1/avgmeasure)*(1/resistance(i)+1/resistance(i+1));
@@ -36,8 +36,29 @@ end
 [eigvals,indices] = sort(real(diag(D)));
 
 %rescale and plot eigenfnc
+eigenfunc = V(:,indices(1));
 
-disp(eigvals(1:2))
+system = -1*eye(length(eigenfunc));
+for i=1:length(eigenfunc)
+    try
+        denom = resistance(i+1)^2*measure(i)*measure(i+1)*(eigenfunc(i)-eigenfunc(i-1))+resistance(i)^2*measure(i)*measure(i+1)*(eigenfunc(i)-eigenfunc(i+1))-resistance(i)*resistance(i+1)*((measure(i)^2+measure(i+1)^2)*eigenfunc(i)+measure(i)*measure(i+1)*(eigenfunc(i-1)+eigenfunc(i+1)));
+        system(i,i+1) = ((measure(i)+measure(i+1))*resistance(i)*resistance(i+1)*eigenfunc(i)*measure(i))/denom;
+        system(i,i-1) = ((measure(i)+measure(i+1))*resistance(i)*resistance(i+1)*eigenfunc(i)*measure(i+1))/denom;
+    catch ME
+       try
+           denom = resistance(i+1)^2*measure(i)*measure(i+1)*(eigenfunc(i)-0)+resistance(i)^2*measure(i)*measure(i+1)*(eigenfunc(i)-eigenfunc(i+1))-resistance(i)*resistance(i+1)*((measure(i)^2+measure(i+1)^2)*eigenfunc(i)+measure(i)*measure(i+1)*(0+eigenfunc(i+1)));
+           system(i,i+1) = ((measure(i)+measure(i+1))*resistance(i)*resistance(i+1)*eigenfunc(i)*measure(i))/denom;           
+       catch ME
+           denom = resistance(i+1)^2*measure(i)*measure(i+1)*(eigenfunc(i)-eigenfunc(i-1))+resistance(i)^2*measure(i)*measure(i+1)*(eigenfunc(i)-0)-resistance(i)*resistance(i+1)*((measure(i)^2+measure(i+1)^2)*eigenfunc(i)+measure(i)*measure(i+1)*(eigenfunc(i-1)+0));
+           system(i,i-1) = ((measure(i)+measure(i+1))*resistance(i)*resistance(i+1)*eigenfunc(i)*measure(i+1))/denom;
+       end
+    end
+end
+
+
+system = [1 zeros(1,length(system)-1);system];
+X =  linsolve(system,[1;zeros(length(system)-1,1)]);
+
 
 %V = V*(1/(max(max(V))));
 %plot(linspace(0,1,4^m-1)',[V(:,10) (measure(2:end).*(1/max(measure)))'])
