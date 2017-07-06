8379a63ed0e50915e7e97c0bf83c7085c8a85e84
Dylan King <kingda16@wfu.edu>
2017-07-06 00:04:01 -0400

    eigenexp is operational and beautiful, if a little slow

--- unstable/gasketcore.m
+++ unstable/gasketcore.m
@@ -1,19 +1,29 @@
-[mu0, mu1, r0, r1] = params(1);
-search = 4;
+r = 0.1;
+[mu0, mu1, r0, r1] = params(r);
+%search = 4;
 
-[smalllaplacian,smallplotting_points,smallpoints] = laplaciangen(1,mu0, r0, r1,0);
-[laplacian,plotting_points,points] = laplaciangen(2,mu0, r0, r1,0);
+%[smalllaplacian,smallplotting_points,smallpoints] = laplaciangen(1,mu0, r0, r1,0);
+[laplacian,plotting_points,points] = laplaciangen(3,mu0, r0, r1,0);
+disp(mu0*r0);
+disp(mu1*r1);
+r = twin(r);
+disp(mu0*r0);
+disp(mu1*r1);
+[mu0, mu1, r0, r1] = params(r);
+[otherlaplacian,plotting_points,points] = laplaciangen(3,mu0, r0, r1,0);
 %[V,D] = partialspectra(laplacian,20);
 [unique_eigvals, eigvals, V] = fullspectra(laplacian);
-[smallunique_eigvals,smalleigvals,smallV,smallindices] = fullspectra(smalllaplacian);
+[unique_eigvals, othereigvals, V] = fullspectra(otherlaplacian);
+output = [eigvals;othereigvals]';
+%[smallunique_eigvals,smalleigvals,smallV,smallindices] = fullspectra(smalllaplacian);
 
 
-[hits diffs hit_indices] = eigenhunt(smallV(:,smallindices(search)),V,20,points,smallplotting_points);
+%[hits diffs hit_indices] = eigenhunt(smallV(:,smallindices(search)),V,20,points,smallplotting_points);
 
 %subplot(1,2,1)
 %gasketgraph(smallplotting_points,smallV(:,2));
 %subplot(1,2,2)
-gasketgraph(plotting_points,V(:,2));
+%gasketgraph(plotting_points,V(:,2));
 
 % for i =1:20
 %     subplot(1,2,1)
