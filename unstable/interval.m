b0608e343f4cf635ce1c8ce5ee6a18738b2ab563
Dylan King <kingda16@charybdis2.math.cornell.edu>
2017-07-07 15:18:49 -0400

    function based interval system

--- unstable/interval.m
+++ unstable/interval.m
@@ -1,59 +1,8 @@
-
-m=5; %level
-
-
-p=0.5; %measure parameter
-q=0.5; %resistance parameter
-cutoff = 1/4;
-
-measure = [p/2 (1-p)/2 (1-p)/2 p/2]; %base measure split
-xcors = [0 1/4 1/2 3/4 1];
-resistance = [q/2 (1-q)/2 (1-q)/2 q/2]; %base resistance split
-%split iteratively...
-for i = 2:m
-    %do a 4x repelem clone, following with a base unit multiplied
-    %to get the desired self similar structure
-    newmeas = [];
-    newres  = [];
-    newxcors = [xcors(1)];
-    for j =1:length(measure)
-        if measure(j)>=cutoff^i
-            newxcors = [newxcors xcors(j)+(xcors(j+1)-xcors(j)).*[1/4 1/2 3/4 1]];
-            newmeas = [newmeas measure(j)*[p/2 (1-p)/2 (1-p)/2 p/2]];
-            newres = [newres resistance(j)*[p/2 (1-p)/2 (1-p)/2 p/2]];
-        else
-            newmeas = [newmeas measure(j)];
-            newres = [newres resistance(j)];
-            newxcors = [newxcors xcors(j+1)];
-        end
-    end
-    measure = newmeas;
-    resistance = newres;
-    xcors = newxcors;
-end
-laplacian = zeros(length(xcors)-2);
-for i = 1:length(xcors)-2
-    avgmeasure = (measure(i)+measure(i+1))/2;
-    laplacian(i,i) = (1/avgmeasure)*(1/resistance(i)+1/resistance(i+1));
-end
-%... and for the right neighbors
-for i = 1:length(xcors)-3
-    avgmeasure = (measure(i)+measure(i+1))/2;
-    laplacian(i,i+1) = (1/avgmeasure)*(-1/resistance(i+1));
-end
-%... and for the left neighbors
-for i = 2:length(xcors)-2
-    avgmeasure = (measure(i)+measure(i+1))/2;
-    laplacian(i,i-1) = (1/avgmeasure)*(-1/resistance(i));
-end
-%compute eigenfunction/values
-[V,D] = eig(laplacian);
-[eigvals,indices] = sort(real(diag(D)));
-
-
+[xcors,laplacian] = intervallapgen(4,0.5,0.5,0); %m,p,q,cutoff
+[~,eigvals,V] = fullspectra(laplacian);
 
 V = V*(1/(max(max(V))));
-plot(xcors(1:end),[0;V(:,indices(3));0])
+plot(xcors(1:end),[0;V(:,1);0])
 
 
 %eigenvalue counting fnc
