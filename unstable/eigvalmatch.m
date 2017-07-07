bf75a70973e2e585421f97f7b2e284ad2ab11c23
Seraphina Lee <el542@cornell.edu>
2017-07-07 09:29:33 -0400

    still debugging

--- unstable/eigvalmatch.m
+++ unstable/eigvalmatch.m
@@ -8,24 +8,13 @@ for i = 1: m
     for j = 1:4
         diff = 10000;
         curr_match = 0;
-        flag = false;
-        disp(prev_mapping(i,j));
-        disp(i);
-        disp(j);
-        disp(' ');
-        
         if (prev_mapping(i,j) ~= 0)
         if prev_eig_high(prev_mapping(i,j)) ~= 0
             for k = 1:length(eig_high)
                 if (abs(eig_high(k) - prev_eig_high(prev_mapping(i,j))) < diff)
                     diff = abs(eig_high(k) - prev_eig_high(prev_mapping(i,j)));
                     curr_match = k;
-                    flag = true;
                 end
-            end
-            if not(flag)
-                disp("hi");
-                disp(diff);
             end
         end
         end
