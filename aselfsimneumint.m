7d50b15dafc92feea5a81b9ad011e5bbbd2f2341
Dylan King <kingda16@charybdis2.math.cornell.edu>
2017-07-25 11:30:01 -0400

    Working aselfsimilar scripts

---
+++ aselfsimneumint.m
@@ -0,0 +1,66 @@
+function [xcors, laplacian,pointmass] = aselfsimneumint( m,ps,cutoff )
+%UNTITLED7 Summary of this function goes here
+%   Detailed explanation goes here
+qs = 1 - ps;
+p = ps(1);
+q = qs(1);
+
+measure = [p/2 (1-p)/2 (1-p)/2 p/2]; %base measure split
+xcors = [0 1/4 1/2 3/4 1];
+resistance = [q/2 (1-q)/2 (1-q)/2 q/2]; %base resistance split
+%split iteratively...
+for i = 2:m
+    %do a 4x repelem clone, following with a base unit multiplied
+    %to get the desired self similar structure
+    p = ps(i);
+    q = qs(i);
+    newmeas = [];
+    newres  = [];
+    newxcors = [xcors(1)];
+    for j =1:length(measure)
+        if measure(j)>=cutoff^i
+            newxcors = [newxcors xcors(j)+(xcors(j+1)-xcors(j)).*[1/4 1/2 3/4 1]];
+            newmeas = [newmeas measure(j)*[p/2 (1-p)/2 (1-p)/2 p/2]];
+            newres = [newres resistance(j)*[p/2 (1-p)/2 (1-p)/2 p/2]];
+        else
+            newmeas = [newmeas measure(j)];
+            newres = [newres resistance(j)];
+            newxcors = [newxcors xcors(j+1)];
+        end
+    end
+    measure = newmeas;
+    resistance = newres;
+    xcors = newxcors;
+end
+laplacian = zeros(length(xcors)-2);
+
+pointmass = zeros(1,length(xcors)-2);
+avgmeasure = (measure(1)+measure(2))/2;
+pointmass(1) = avgmeasure;
+laplacian(1,1) = (1/avgmeasure)*(1/resistance(2));
+avgmeasure = (measure(end-1)+measure(end))/2;
+pointmass(end) = avgmeasure;
+laplacian(end,end) = (1/avgmeasure)*(1/resistance(end));
+
+for i = 2:length(xcors)-3
+    avgmeasure = (measure(i)+measure(i+1))/2;
+    laplacian(i,i) = (1/avgmeasure)*(1/resistance(i)+1/resistance(i+1));
+    pointmass(i) = avgmeasure;
+end
+%... and for the right neighbors
+for i = 1:length(xcors)-3
+    avgmeasure = (measure(i)+measure(i+1))/2;
+    laplacian(i,i+1) = (1/avgmeasure)*(-1/resistance(i+1));
+end
+%... and for the left neighbors
+for i = 2:length(xcors)-2
+    avgmeasure = (measure(i)+measure(i+1))/2;
+    laplacian(i,i-1) = (1/avgmeasure)*(-1/resistance(i));
+end
+
+pointmass = [measure(1) pointmass measure(end)];
+
+end
+
+
+
