a1a0cb94c56e0354a27c20253346511471249bfe
Dylan King <kingda16@wfu.edu>
2017-08-02 14:35:23 -0400

    limit data done

--- unstable/limiteigfns/csvconcat.py
+++ unstable/limiteigfns/csvconcat.py
@@ -4,9 +4,9 @@ for p in ['1','2','3','4','5','6','7','8']:
     c = open('./3/'+p+'/eigvals.csv')
     d = open('./4/'+p+'/eigvals.csv')
     e = open('./5/'+p+'/eigvals.csv')
-    output = open('./output/'+p+'.csv','a')
-    output.write('m=1,m=2,m=3,m=4,m=5,p='+str(10^-int(p)))
+    output = open('./output/'+p+'.csv','w+')
+    output.write('m=1,m=2,m=3,m=4,m=5,p='+str(pow(10,-int(p)))+'\n')
     for i in range(0,1092):
-        output.write(a.readline()+','+b.readline()+','+c.readline()+','+d.readline()+','+e.readline())
+        output.write(a.readline()[:-1]+','+b.readline()[:-1]+','+c.readline()[:-1]+','+d.readline()[:-1]+','+e.readline()[:-1]+'\n')
     output.close()
     
--- unstable/webprinter.m
+++ unstable/webprinter.m
@@ -2,31 +2,64 @@
 
 
 
-pvals=[10^-1 10^-2 10^-3 10^-4 10^-5 10^-6 10^-7 10^-8];
-pvals = 1-pvals;
-mvals=[1,2,3,4,5];
+
+rvals=[10^-1 10^-2 10^-3 10^-4 10^-5 10^-6 10^-7 10^-8];
+mvals=[1,2,3];
 
 for k=1:length(mvals)
     m=mvals(k);
-    mkdir('./limiteigfns',num2str(m))
-        for j=1:length(pvals)
-            p=pvals(j);
-            [laplacian,xcors] = laplaciangen(m,p,0,'i','d'); %m,p,q,cutoff
-            [x,eigvals,V] = fullspectra(laplacian);        
-            mkdir(strcat('./limiteigfns/',num2str(m)),num2str(abs(log10(1-p))))
-            csvwrite(strcat('./limiteigfns/', num2str(m), '/', num2str(abs(log10(1-p))), '/', 'eigvals.csv'),eigvals')
-%         for i=1:(length(V)-mod(length(V),6))/6+1
-%             for l=1:6
-%                 index=(i-1)*6+l;
-%                 filename=num2str(i);
-%                 if(index < length(V)+1)
-%                     subplot(3,2,l)
-%                     plot(xcors(1:end),(V(1,index)/abs(V(1,index))).*[0;V(:,index);0])
-%                     xlabel(strcat("m=",num2str(m), " p=",num2str(p), " \lambda_{",num2str(index),"}=",num2str(eigvals(index))))
-%                 end
-%             end
-%             print(strcat('.\limiteigfns\', num2str(m), '\', num2str(abs(log10(1-p))), '\', filename),'-dpdf', '-bestfit')
-%             clf
-%         end
+    mkdir('./limiteigfns',num2str(m));
+    for j=1:length(rvals)
+        r=rvals(j);
+        mkdir(strcat('./limiteigfns/',num2str(m)),num2str(abs(log10(r))));
+        [ laplacian,plotting_points,points,cells ] = laplaciangen( m,r,0,'g','d');
+        [unique_eigvals, eigvals, V] = fullspectra(laplacian);
+        for i=1:length(V)
+            filename=num2str(i);
+            gasketgraph(plotting_points,V(:,i));
+            xlabel(strcat("m=",num2str(m), " r=",num2str(r), " \lambda_{",num2str(i),"}=",num2str(eigvals(i))))
+            savefig(gcf,strcat('./limiteigfns/', num2str(m), '/', num2str(j), '/', filename, '.fig'))
+            clf
+        end
     end
 end
+
+
+
+
+
+
+
+
+
+
+
+% 
+% 
+% pvals=[10^-1 10^-2 10^-3 10^-4 10^-5 10^-6 10^-7 10^-8];
+% mvals=[1,2,3,4,5];
+% 
+% for k=1:length(mvals)
+%     m=mvals(k);
+%     mkdir('./limiteigfns',num2str(m))
+%         for j=1:length(pvals)
+%             p=pvals(j);
+%             [laplacian,xcors] = laplaciangen(m,p,0,'i','d'); %m,p,q,cutoff
+%             [x,eigvals,V] = fullspectra(laplacian);        
+%             mkdir(strcat('./limiteigfns/',num2str(m)),num2str(abs(log10(p))))
+%             csvwrite(strcat('./limiteigfns/', num2str(m), '/', num2str(abs(log10(p))), '/', 'eigvals.csv'),eigvals')
+% %         for i=1:(length(V)-mod(length(V),6))/6+1
+% %             for l=1:6
+% %                 index=(i-1)*6+l;
+% %                 filename=num2str(i);
+% %                 if(index < length(V)+1)
+% %                     subplot(3,2,l)
+% %                     plot(xcors(1:end),(V(1,index)/abs(V(1,index))).*[0;V(:,index);0])
+% %                     xlabel(strcat("m=",num2str(m), " p=",num2str(p), " \lambda_{",num2str(index),"}=",num2str(eigvals(index))))
+% %                 end
+% %             end
+% %             print(strcat('.\limiteigfns\', num2str(m), '\', num2str(abs(log10(1-p))), '\', filename),'-dpdf', '-bestfit')
+% %             clf
+% %         end
+%     end
+% end
